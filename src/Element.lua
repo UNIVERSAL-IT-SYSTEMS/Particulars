472b4f2931139305a72091344f5f0eab63484292
Paul Liverman <paul.liverman.iii@gmail.com>
2015-11-24 11:12:26 -0800

    dumb typo fix

--- src/Element.lua
+++ src/Element.lua
@@ -2,9 +2,9 @@ local class = require "lib.middleclass"
 
 local Element = class("Element")
 
-Double.static.maxCount = 100 --1000
-Double.static.generated = 0
-Double.static.count = 0
+Element.static.maxCount = 100 --1000
+Element.static.generated = 0
+Element.static.count = 0
 
 function Element:initialize(double)
     -- DO SHIT BASED ON POSITIVE OR NEGATIVE
